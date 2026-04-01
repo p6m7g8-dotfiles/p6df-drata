2d74a26a3d30bc3e5dc3d5a1866a7f33121efef9
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-01 15:41:21 -0400

    fix(drata): replace prompt::mod and profile::on/off with profile::mod, add p6common dep, add README (#1)
    * fix(drata): replace prompt::mod and profile::on/off with profile::mod using p6_return_words, add p6common dep, add README
    
    * fix(prompt): use double quotes for variable expansion in p6_return_words (SC2016)
    
    * chore(ci): add standard github workflows
    
    * fix(drata): restore single-quoted variable in profile::mod

--- init.zsh
+++ init.zsh
@@ -7,70 +7,21 @@
 #>
 ######################################################################
 p6df::modules::drata::deps() {
-  ModuleDeps=()
+  ModuleDeps=(p6m7g8-dotfiles/p6common)
 }
 
 ######################################################################
 #<
 #
-# Function: str str = p6df::modules::drata::prompt::mod()
+# Function: words drata $DRATA_API_KEY = p6df::modules::drata::profile::mod()
 #
 #  Returns:
-#	str - str
+#	words - drata $DRATA_API_KEY
 #
-#  Environment:	 P6_DFZ_PROFILE_DRATA
+#  Environment:	 DRATA_API_KEY
 #>
 ######################################################################
-p6df::modules::drata::prompt::mod() {
-  local str=""
-  local profile="$P6_DFZ_PROFILE_DRATA"
+p6df::modules::drata::profile::mod() {
 
-  if p6_string_blank_NOT "$profile"; then
-    str="drata:\t\t  ${profile}"
-  fi
-
-  p6_return_str "$str"
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::drata::profile::on(profile, code)
-#
-#  Args:
-#	profile -
-#	code - shell code block
-#
-#  Environment:	 P6_DFZ_PROFILE_DRATA
-#>
-######################################################################
-p6df::modules::drata::profile::on() {
-  local profile="$1"
-  local code="$2"
-
-  p6_run_code "$code"
-
-  p6_env_export "P6_DFZ_PROFILE_DRATA" "$profile"
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::drata::profile::off(code)
-#
-#  Args:
-#	code - shell code block previously passed to profile::on
-#
-#  Environment:	 P6_DFZ_PROFILE_DRATA
-#>
-######################################################################
-p6df::modules::drata::profile::off() {
-  local code="$1"
-
-  p6_env_unset_from_code "$code"
-  p6_env_export_un P6_DFZ_PROFILE_DRATA
-
-  p6_return_void
+  p6_return_words 'drata' '$DRATA_API_KEY'
 }
